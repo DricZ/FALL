dc6a266038f8925f693cdd52a1f9df0661a4330f
tansu317 <95393211+tansu317@users.noreply.github.com>
2022-12-30 17:35:17 +0700

    paling new

--- app/src/main/java/com/example/fall/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/fall/ui/explore/ExploreFragment.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.fall.databinding.FragmentDashboardBinding
+import com.example.fall.databinding.FragmentExploreBinding
 
 class ExploreFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentExploreBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class ExploreFragment : Fragment() {
         val exploreViewModel =
             ViewModelProvider(this).get(ExploreViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 //        val textView: TextView = binding.textDashboard
