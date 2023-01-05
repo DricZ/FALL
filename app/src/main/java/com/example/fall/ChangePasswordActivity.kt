29ee853a8f77ae7b26c26e4b55470bed33785726
tansu317 <95393211+tansu317@users.noreply.github.com>
2023-01-05 11:05:13 +0700

    new pass done

--- app/src/main/java/com/example/fall/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/fall/ChangePasswordActivity.kt
@@ -6,22 +6,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
 
+        val sp = getSharedPreferences("SessionUser", MODE_PRIVATE)
+        val savedName = sp.getString("id_user", "")
+
+        val db = FirebaseFirestore.getInstance()
+
         val btnSavePass = findViewById<Button>(R.id.buttonSavePass)
         val etNewPass =  findViewById<EditText>(R.id.editTextNewPass)
         val etRePass = findViewById<EditText>(R.id.editTextRePass)
+
+
         btnSavePass.setOnClickListener {
 //            UBAH DAN SAVE PASSWORD BARU KE DB
             if (etNewPass.text.toString() == etRePass.text.toString()){
-                etNewPass.setText("")
-                etRePass.setText("")
-                startActivity(Intent(this@ChangePasswordActivity, tesHome::class.java))
-
+                val id = db.collection("account").document(savedName.toString())
+                id.update("password", etNewPass.text.toString())
+                    .addOnSuccessListener {
+                        etNewPass.setText("")
+                        etRePass.setText("")
+                        startActivity(Intent(this@ChangePasswordActivity, tesHome::class.java))
+                    }
             }
             else{
                 etRePass.setText("")
